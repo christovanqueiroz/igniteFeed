855b760d20f92845282d87de960865ff5b1f7b46
Christovan Queiroz <christovanbrumqueiroz@gmail.com>
2024-05-25 11:37:45 -0300

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.module.css 	new file:   src/App.tsx 	new file:   src/assets/ignite-logo.svg 	new file:   src/components/Avatar.module.css 	new file:   src/components/Avatar.tsx 	new file:   src/components/Comment.module.css 	new file:   src/components/Comment.tsx 	new file:   src/components/Header.module.css 	new file:   src/components/Header.tsx 	new file:   src/components/Post.module.css 	new file:   src/components/Post.tsx 	new file:   src/components/Sidebar.module.css 	new file:   src/components/Sidebar.tsx 	new file:   src/global.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+import { useState } from 'react';
+
+type CommentProps = {
+    content: string,
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/christovanqueiroz.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Christovan Queiroz</strong>
+                            <time title="04 de Maio às 10h05" dateTime="2024-05-04 10:05:34">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
